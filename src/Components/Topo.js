f39037576216b27ebeda2c41c8903731016c4123
Luan Menezes <menezluan120@gmail.com>
2025-05-30 07:39:35 -0300

    Update Topo.js

--- src/Components/Topo.js
+++ src/Components/Topo.js
@@ -1,25 +1,21 @@
 import React from "react";
 
 export default function Topo() {
-  const imagemInicial = "https://wallpapers.com/images/hd/smiling-bloody-trafalgar-law-one-piece-fanart-44bsvjb9q10npjlr.jpg";
+  const imagemInicial =
+    "https://wallpapers.com/images/hd/smiling-bloody-trafalgar-law-one-piece-fanart-44bsvjb9q10npjlr.jpg";
 
   const [nome, setNome] = React.useState("");
   const [avatar, setAvatar] = React.useState(imagemInicial);
 
-
-    
-
   function InserirNome() {
-     const nomeDigitado = prompt("Qual o seu nome?");
-     setNome(nomeDigitado);
+    const nomeDigitado = prompt("Qual o seu nome?");
+    setNome(nomeDigitado);
   }
   function mudaAvatar() {
-     const linkAvatar = prompt("Qual o seu nome?");
-     setAvatar(linkAvatar);
+    const linkAvatar = prompt("Insira o Url da Imagem");
+    setAvatar(linkAvatar ? linkAvatar : imagemInicial);
   }
 
-
-  
   return (
     <div className="topo">
       <h1>FilmesFlix</h1>
